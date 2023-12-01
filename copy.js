ba56a7552010f053710c8bf3601040f8325b9356
Cayla <cayla.cardiff@gmail.com>
2023-12-01 11:56:17 -0600

    first commit - of what, two?

---
+++ copy.js
@@ -0,0 +1,24 @@
+// It needs to wait until the thing loads, find the thing, add a click, and copy to clipboard onclick.
+
+// NONE OF THESE WORK:
+// #1: 
+// document.getElementById('btn').addEventListener('click', copyVague()); getElement returns null.
+// #2:
+// document.querySelector("#btn").addEventListener("click", copyVague()); querySelector returns null.
+// #3:
+// document.addEventListner("DOMContentLoaded", function() {
+//   document.querySelector("#btn").addEventListener("click", copyVague());
+// }); says document.addEventListener is not a function.  (document is null probably?) 
+// #4: 
+// document.addEventListner("DOMContentLoaded", function() {
+//     document.getElementById('btn').addEventListener("click", copyVague());
+//   }); same old same.
+
+function copyVague() {
+  var copyText = document.getElementById("phrase");
+  console.log(copyText.innerHTML)
+  navigator.clipboard.writeText(copyText.innerHTML);
+};
+
+// Bonus: then close the popup after copy?
+
