93999f89b5358e4fe4692076567779213c233e09
Cayla <cayla.cardiff@gmail.com>
2025-06-16 16:59:06 -0500

    Added weirdChars & genericName buttons.

--- weirdChars.js
+++ weirdChars.js
@@ -1,4 +1,4 @@
 document.getElementById("weirdChars").addEventListener("click", () => {
-  const text = "Hello, 世界! こんにちは 안녕하세요 𝒜𝓃𝒹𝓇é مرحبا‏‎ ‍‎​‎​‎​(     ) –—…“”‘’✓©®𝌆𠀋𡃁"
+  const text = "Hello, 世界! こんにちは 안녕하세요 𝒜𝓃𝒹𝓇é مرحبا 👋‍ 😀(     ) –—…“”‘’✓©®𝌆𠀋𡃁"
   navigator.clipboard.writeText(text);
 });
